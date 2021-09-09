efdd1580a07efc92dcaac30159b0199d8c57c589
김경호 <kyeongho94@gmail.com>
2021-09-09 15:54:30 +0900

    BoardDetail UI

--- src/screen/units/BoardDetailPage/BoardDetailPage.presenter.tsx
+++ src/screen/units/BoardDetailPage/BoardDetailPage.presenter.tsx
@@ -1,6 +1,19 @@
 import {
   Wrapper,
   BackImage,
+  Detail__Wrapper,
+  Back,
+  Top,
+  Top__Left,
+  Title,
+  Top__Right,
+  Edit,
+  Delete,
+  User__Info,
+  Avatar,
+  User__Info__Right,
+  Name,
+  Date
 } from './BoardDetailPage.styles';
 import React from 'react';
 // import MapView from 'react-native-maps';
@@ -9,7 +22,31 @@ export default function BoardDetailPageUI() {
   return (
     <>
       <Wrapper>
-        <BackImage></BackImage>
+        <BackImage>
+          <Detail__Wrapper>
+            <Back></Back>
+            <Top>
+              <Top__Left>
+                <Title>여행 동행 구합니다.</Title>
+              </Top__Left>
+
+              <Top__Right>
+                <Edit></Edit>
+                <Delete></Delete>
+              </Top__Right>
+            </Top>
+
+            <User__Info>
+              <Avatar></Avatar>
+
+              <User__Info__Right>
+                <Name>호두자두</Name>
+                <Date>2021. 08. 31</Date>
+              </User__Info__Right>
+            </User__Info>
+
+          </Detail__Wrapper>
+        </BackImage>
       </Wrapper>
     </>
   );
--- src/screen/units/BoardDetailPage/BoardDetailPage.styles.ts
+++ src/screen/units/BoardDetailPage/BoardDetailPage.styles.ts
@@ -4,6 +4,86 @@ export const Wrapper = styled.View``;
 
 export const BackImage = styled.View`
   width: 100%;
-  height: 297px;
+  height: 224px;
   background-color: #999;
 `;
+
+export const Detail__Wrapper = styled.View`
+  width: 100%;
+  height: 1413px;
+  background-color: #fff;
+  margin-top: 30px;
+  padding: 0 20px;
+  border-radius: 30px 30px 0 0;
+`
+
+export const Back = styled.View`
+  width: 6px;
+  height: 10px;
+  background-color: #FFBE2B;
+  margin: 30px 0 17px 0;
+`
+
+export const Top = styled.View`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin-bottom: 11px;
+`
+
+export const Title = styled.Text`
+  font-size: 18px;
+  line-height: 20px;
+  /* letter-spacing: -0.02em; */
+`
+
+export const Top__Left = styled.View``
+
+export const Edit = styled.View`
+  width: 14px;
+  height: 14px;
+  background-color: #FFBE2B;
+  margin-right: 10px;
+`
+
+export const Top__Right = styled.View`
+  display: flex;
+  flex-direction: row;
+`
+
+
+export const Delete = styled.View`
+  width: 13px;
+  height: 15px;
+  background-color: #FFBE2B;
+`
+
+export const User__Info = styled.View`
+  display: flex;
+  flex-direction: row;
+  margin-bottom: 14.98px;
+`
+
+export const Avatar = styled.View`
+  width: 24px;
+  height: 24px;
+  background-color: #FFBE2B;
+  margin-right: 7px;
+  border-radius: 50px 50px 50px 50px;
+`
+
+export const User__Info__Right = styled.View`
+  /* display: flex; */
+`
+
+export const Name = styled.Text`
+  font-size: 11px;
+  line-height: 13px;
+  /* letter-spacing: -0.02em; */
+`
+
+export const Date = styled.Text`
+  font-size: 7.6px;
+  line-height: 11px;
+  color: rgba(0, 0, 0, 0.5);
+`
